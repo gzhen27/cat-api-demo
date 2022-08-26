6ddbf69bc18aa6166c82e993886ce16e72dc436f
Gray <grayzprogrammer@gmail.com>
2022-08-26 13:44:09 -0500

    Initial Commit

---
+++ CatAPIDemo/CatAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIDemoApp.swift
+//  CatAPIDemo
+//
+//  Created by G Zhen on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
