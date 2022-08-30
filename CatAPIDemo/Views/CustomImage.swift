28302658e72c5e429b731d6ade68b84bd54907d9
Gray <grayzprogrammer@gmail.com>
2022-08-29 20:08:35 -0500

    adjusts the custom image view

--- CatAPIDemo/Views/CustomImage.swift
+++ CatAPIDemo/Views/CustomImage.swift
@@ -28,38 +28,35 @@ struct CustomImage: View {
     }
     
     var body: some View {
-        if let imageUrl = url {
-            AsyncImage(url: imageUrl) { phase in
-                if let image = phase.image {
-                    // temporary fix for the ratio
-                    if imageRatio == .scaleToFill {
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: size.width, height: size.height)
-                            .clipped()
-                            .cornerRadius(cornerRadius)
-                    } else {
-                        image
-                            .resizable()
-                            .cornerRadius(cornerRadius)
-                            .scaledToFit()
-                            .frame(width: size.width, height: size.height)
-                            .clipped()
-                    }
-                } else if phase.error != nil {
-                    Color.red
+        if url != nil {
+            AsyncImage(url: url) { image in
+                if imageRatio == .scaleToFill {
+                    image
+                        .resizable()
+                        .scaledToFill()
                         .frame(width: size.width, height: size.height)
+                        .clipped()
                         .cornerRadius(cornerRadius)
                 } else {
-                    ProgressView()
+                    image
+                        .resizable()
+                        .cornerRadius(cornerRadius)
+                        .scaledToFit()
                         .frame(width: size.width, height: size.height)
+                        .clipped()
                 }
+            } placeholder: {
+                ProgressView()
+                    .frame(width: size.width, height: size.height)
             }
         } else {
-            Color.gray
-                .frame(width: size.width, height: size.height)
-                .cornerRadius(cornerRadius)
+            ZStack {
+                Color.gray
+                    .frame(width: size.width, height: size.height)
+                    .cornerRadius(cornerRadius)
+                Text("No Image")
+                    .bold()
+            }
         }
     }
 }
@@ -67,12 +64,9 @@ struct CustomImage: View {
 struct CustomImage_Previews: PreviewProvider {
     static var previews: some View {
         let size = CGSize(width: 100, height: 100)
-//        let url = URL(string: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg")
         Group {
             CustomImage(size: size, url: nil, cornerRadius: 10)
-                .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/))
-//            CustomImage(size: size, url: url)
-//                .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/))
+                .previewLayout(.fixed(width: size.width, height: size.height))
         }
     }
 }
